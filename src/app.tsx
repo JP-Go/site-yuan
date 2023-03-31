80bcb98ae13271f12210ef9b4446d324114323c6
João Pedro <jpedro1580@gmail.com>
2023-03-31 10:55:53 -0300

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import preactLogo from './assets/preact.svg'
+
+export function App() {
+  return (
+    <h1 className="text-sky-300 text-3xl">Hello world</h1>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{tsx,js,jsx}','./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+        yuanYellow: "",
+      }
+    },
+  },
+  plugins: [],
+}
+
