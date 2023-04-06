167044c8ad55f3724fe575fc92dc8b9861b1882b
João Pedro <jpedro1580@gmail.com>
2023-04-06 13:37:08 -0300

    Rename component to improve component semantics

--- src/components/Header/HeaderSectionLink.tsx
+++ src/components/Header/HeaderSectionLink.tsx
@@ -1,12 +1,14 @@
-import { HeaderSection } from "."
+import type { NavigationSection } from "../../@types/sections";
 
 type HeaderSectionLinkProps = {
-    section: HeaderSection
-}
+  section: NavigationSection;
+};
 
-export function HeaderSectionLink({ section }: HeaderSectionLinkProps) {
-    return <a href={`#${section.link}`} className="hidden mr-px cursor-pointer group lg:block" >
-        <span className="font-bold uppercase text-primary" >{section.title}</span>
-        <div className="bg-yuan-yellow w-0 transition-all duration-[400] group-hover:w-full h-0.5"></div>
+export function NavigationSectionLink({ section }: HeaderSectionLinkProps) {
+  return (
+    <a href={`#${section.link}`} className="flex-initial cursor-pointer group">
+      <span className="font-bold uppercase text-primary">{section.title}</span>
+      <div className="bg-yuan-yellow w-0 transition-all duration-[400] group-hover:w-full h-0.5"></div>
     </a>
+  );
 }
