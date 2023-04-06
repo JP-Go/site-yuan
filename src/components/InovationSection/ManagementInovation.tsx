f02b318b85d90dc998b8545644fa014e828b8dd5
João Pedro <jpedro1580@gmail.com>
2023-04-06 13:36:06 -0300

    More responsiveness changes

--- src/components/InovationSection/ManagementInovation.tsx
+++ src/components/InovationSection/ManagementInovation.tsx
@@ -6,14 +6,14 @@ export function ManagementInovation() {
       <div className="flex flex-col gap-2 items-center px-8 py-2 mx-auto w-full h-full lg:p-10 lg:flex-row lg:w-4/5 lg:gap-14">
         <img
           alt="inovationImage"
-          className="md:w-1/2 max-w-[500px] w-40 lg:h-full h-40"
+          className="md:w-1/2 max-w-[500px] w-1/2 lg:h-full h-60"
           src={inovacaoGestaoEmpresarialImg}
         />
         <div className="flex flex-col gap-4 max-w-1/2">
-          <h2 className="text-lg font-bold lg:text-6xl text-yuan-yellow">
+          <h2 className="text-xl font-bold lg:text-6xl text-yuan-yellow">
             Inovação na Gestão Empresarial
           </h2>
-          <p className="text-sm text-white lg:text-2xl">
+          <p className="text-base text-white lg:text-2xl">
             A Yuan Soluções oferece uma ampla variedade de serviços para atender
             às necessidades de seus clientes, desde consultoria estratégica e
             gerenciamento de projetos até desenvolvimento de software e soluções
@@ -21,7 +21,7 @@ export function ManagementInovation() {
           </p>
           <a
             href="#proposta"
-            className="py-4 w-full max-w-md font-bold text-center rounded-lg cursor-pointer md:w-4/5 lg:text-3xl text-inovation bg-yuan-yellow"
+            className="p-4 font-bold text-center rounded-lg cursor-pointer w-fit lg:w-full md:max-w-4/5 lg:text-3xl text-inovation bg-yuan-yellow"
           >
             Fale com o especialista!
           </a>
